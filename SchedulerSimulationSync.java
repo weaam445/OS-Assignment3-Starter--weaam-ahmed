8db48cf882ad16d2f0026c2df3b7840f5bff94fc
weaam445 <445052276@std.psau.edu.sa>
2026-05-06 17:28:49 +0300

    Add locking mechanism to addWaitingTime method

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -68,8 +68,12 @@ class SharedResources {
     
     // Method to add waiting time
     public static void addWaitingTime(long time) {
-        // TODO: Protect this critical section with a lock
-        totalWaitingTime += time;
+        waitingTimeLock.lock();
+        try{
+            totalWaitingTime +=time;
+        }finally{
+            waitingTimeLock.unlock();
+        }
     }
     
     // Method to log execution
