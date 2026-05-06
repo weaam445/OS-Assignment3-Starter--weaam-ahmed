7bed76e1c699beac9e12fde9ba9ec36b70654963
weaam445 <445052276@std.psau.edu.sa>
2026-05-06 17:12:55 +0300

    Update SchedulerSimulationSync.java

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -57,8 +57,13 @@ class SharedResources {
     }
     // Method to increment completed process counter
     public static void incrementCompletedProcess() {
-        // TODO: Protect this critical section with a lock
-        completedProcessCount++;
+        completedProcessLock.lock();
+        try{
+             completedProcessCount++;
+        }finally{
+            completedProcessLock.unlock();        
+        
+        }
     }
     
     // Method to add waiting time
