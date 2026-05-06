5cfdd0677722a1270f10fd838264825dfcecbd7a
weaam445 <445052276@std.psau.edu.sa>
2026-05-06 17:32:49 +0300

    Implement thread safety in logExecution method
    Added locking mechanism to protect critical section in logExecution method.

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -78,12 +78,14 @@ class SharedResources {
     
     // Method to log execution
     public static void logExecution(String message) {
-        // TODO: Protect this critical section with a lock
-        // RACE CONDITION: ArrayList is not thread-safe!
-        executionLog.add(message);
-    }
+        logLok.lock();
+        try{
+            executionLog.add(message);
+        } finally{
+   logLock.unlock();
+        }
 }
-
+}
 // Class representing a process that implements Runnable to be run by a thread
 class Process implements Runnable {
     private String name;
